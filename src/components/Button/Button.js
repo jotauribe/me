9623178034ff52638a2af70b737f613de6ed6892
Jhonatan Uribe Echenique <jhonatan.uribe@globants-MacBook-Pro-2.local>
2019-04-20 12:04:56 -0500

    [EXPERIENCE SECTION] - Updating Job Card design and changing components folder names

---
+++ src/components/Button/Button.js
@@ -0,0 +1,28 @@
+import React from "react";
+import styled from "styled-components";
+
+const Button = styled.button`
+  height: 32px;
+  border-radius: 16px;
+  padding-top: 6px;
+  padding-bottom: 6px;
+  padding-right: 16px;
+  padding-left: 16px;
+  font-weight: 700;
+  line-height: 20px;
+  border: 0;
+  background-color: var(--theme-color-accent);
+  display: flex;
+  color: white;
+  box-shadow: 0 2px 4px 0 rgba(60, 64, 67, 0.302),
+    0 1px 3px 1px rgba(60, 64, 67, 0.149);
+  transition: box-shadow 0.8s;
+  &:hover {
+    box-shadow: 0 4px 6px 0 rgba(60, 64, 67, 0.302),
+      0 4px 8px 3px rgba(60, 64, 67, 0.149);
+  }
+`;
+
+export default function ButtonComponent({ children, style }) {
+  return <Button style={style}>{children}</Button>;
+}
---
+++ src/components/Experience/Experience.js
@@ -0,0 +1,34 @@
+import React from "react";
+import styled from "styled-components";
+
+import data from "../../data";
+import Section from "../section";
+import Job from "../job";
+import Timeline from "../timeline";
+
+const ExperienceBody = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  align-items: flex-start;
+`;
+
+export default function WorkExperience() {
+  const jobs = data.experience;
+
+  return (
+    <Section className="experience">
+      <Section.Title>EXPERIENCE</Section.Title>
+      <ExperienceBody className="experience__body">
+        <Timeline>
+          {jobs &&
+            jobs.map(job => (
+              <Timeline.Point>
+                <Job {...job} />
+              </Timeline.Point>
+            ))}
+        </Timeline>
+      </ExperienceBody>
+    </Section>
+  );
+}
